e264552988332105770e324c8d60b4ba50983fe8
deepaakhil84 <deepaakhiltvm@gmail.com>
2022-01-26 23:41:13 +0000

    state & life cycle methods

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  function add(num1, num2) {
-    const sum = num1 + num2;
-    return sum;
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//   return <div>longitude:</div>;
+// };
+// class App extends React.Component {
+//constructor is the first fn called when instance of the class is created
+// constructor(props) {
+//   //extending the component base class constructor
+//   super(props);
+//   //initializing state
+//   this.state = {
+//     lat: null,
+//     errorMessage: "",
+//   };
+//   // window.navigator.geolocation.getCurrentPosition(
+//   (position) => {
+//     this.setState({ lat: position.coords.latitude });
+//   },
+//   (err) => {
+//     this.setState({ errorMessage: err.message });
+//   }
+// );
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    console.log("my component rendered");
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
-  useEffect(() => {
-    console.log(add(1, 2));
-  }, [add]);
-  return (
-    <div>
-      <input type="text" id="num1" name="num1" />
-    </div>
-  );
-};
+  componentDidUpdate() {
+    console.log("my component is updated");
+  }
+
+  //required render function for a class
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Latitude:{this.state.lat}</div>;
+    }
+    return <div>loading</div>;
+  }
+}
 ReactDom.render(<App />, document.querySelector("#root"));
---
+++ src/seasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const seasonDisplay = () => {
+  return <div>Season Display</div>;
+};
+export default seasonDisplay;
